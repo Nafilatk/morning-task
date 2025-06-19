4870647d200530d5950821ba85c36c7d83926ad9
nafila <nafilatk7.com>
2025-06-19 12:30:44 +0530

    swap

---
+++ task5.js
@@ -0,0 +1,14 @@
+
+
+// let num=[1,1,2]
+// let nums = new Set ([1,1,2])
+// let a=[...nums]
+
+// console.log(a);
+// console.log(num[2]);
+
+
+// let arr=[0,0,1,1,1,2,2,3,3,4]
+// let select=[...new Set(arr)]
+// console.log(select);
+
---
+++ task6.js
@@ -0,0 +1,10 @@
+
+let a=5
+function naf(a,b){
+    let c=a
+    a=b
+    b=c
+    console.log('a:',a,'b:',b);
+}
+     
+naf(5,10)
